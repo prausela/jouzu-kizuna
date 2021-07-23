c2c9206d60a5b185933186fb53c78ce1abc74da9
Paula Oseroff <oseroffp@gmail.com>
2021-07-23 01:56:08 -0300

    Add AnswerUI component
    AnswerUI is a button with a possible answer. The user shall click it if they think it's the correct answer

---
+++ src/UI/AnswerUI.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+const AnswerUI = (props) => {
+    const className = props.className ? props.className + " d-grid p-2" : "d-grip p-2";
+    const text = props.text;
+    return (
+        <div className={className}>
+            <Button variant="primary">{text}</Button>
+        </div>
+    )
+}
+
+export default AnswerUI;
--- src/UI/Interface.js
+++ src/UI/Interface.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './Interface.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AnswerUI from './AnswerUI';
 
-const Interface = () => {
+const Interface = (props) => {
+    const answers = [
+        {text : props.ans1 ? props.ans1 : "", className : "ans1"}, 
+        {text : props.ans2 ? props.ans2 : "", className : "ans2"}, 
+        {text : props.ans3 ? props.ans3 : "", className : "ans3"}, 
+        {text : props.ans4 ? props.ans4 : "", className : "ans4"}, 
+    ]
     return (
         <div className="interface">
             <div className="menu"></div>
             <div className="question"></div>
-            <div className="ans1"></div>
-            <div className="ans2"></div>
-            <div className="ans3"></div>
-            <div className="ans4"></div>
+            {
+                answers.map(ans => (
+                    <AnswerUI text={ans.text} className={ans.className}/>
+                ))
+            }
         </div>
     )
 }
