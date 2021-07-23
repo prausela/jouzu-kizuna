ed65d53ae0dcb69d194a0fd21ffd1a9b9dc9261f
Paula Oseroff <oseroffp@gmail.com>
2021-07-23 18:24:09 -0300

    Fix returned values in KanjiToHiraganaDAO
    getQuestions now returns empty array if there are no questions and getPossibleAnswers returns undefined either if there is no such question or there is no correct answer. If there are no incorrect answers returns incorrect as an empty array

--- src/data/KanjiToHiraganaDAO.js
+++ src/data/KanjiToHiraganaDAO.js
@@ -1,14 +1,19 @@
 import KanjiToHiragana from './kanji_to_hiragana.json';
 
 const getQuestions = () => {
-    return KanjiToHiragana.map(ans => ans.question);
+    return KanjiToHiragana.map(ans => ans.question) || [];
 }
 
 const getPossibleAnswers = (question) => {
     const answerObject = KanjiToHiragana.find(ans => ans.question === question);
+
+    if(!answerObject || !answerObject.correct_answer){
+        return undefined;
+    }
+
     return {
         correct     : answerObject.correct_answer,
-        incorrect   : answerObject.other_answers
+        incorrect   : answerObject.other_answers ? answerObject.other_answers : []
     }
 }
 
