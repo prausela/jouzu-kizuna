1ba4a2c34268e6eb0a3a8b60341bbf8612ccc582
Paula Oseroff <oseroffp@gmail.com>
2021-07-26 23:29:34 -0300

    Refactor Interface into more scoped styles and add logic behind multiple screens

--- src/UI/Interface.js
+++ src/UI/Interface.js
@@ -1,18 +1,33 @@
 // From packages
-import React from 'react';
+import React, { useState } from 'react';
 
 // Style files
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Interface.css';
 
 // Controllers
-import GameController from './GameController';
 //import MainMenuController from './MainMenuController';
+import GameController from './GameController';
 
 const Interface = (props) => {
+    const interfaces = {
+        /*"main_menu" : {
+            className   : "main-menu-interface",
+            controller  : MainMenuController
+        },*/
+        "game"      : {
+            className   : "game-interface",
+            controller  : GameController
+        }
+    }
+
+    const [ currInterface, setCurrInterface ] = useState("game");
+
+    const className     = "interface " + interfaces[currInterface].className;
+    const Controller    = interfaces[currInterface].controller
     return (
-        <div className="interface">
-            <GameController />
+        <div className={className}>
+            <Controller />
         </div>
     )
 }
