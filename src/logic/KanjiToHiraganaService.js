d7e8f06b7e84d514887ed59598e12a8dcfe51627
Paula Oseroff <oseroffp@gmail.com>
2021-07-27 17:55:38 -0300

    Refactor KanjiToHiraganaService into _QuestionsWithPossibleAnswersService

--- src/logic/KanjiToHiraganaService.js
+++ src/logic/KanjiToHiraganaService.js
@@ -1,65 +1,8 @@
+import _QuestionsWithPossibleAnswersService from "./_QuestionsWithPossibleAnswersService";
 import KanjiToHiraganaDAO from "../data/access/KanjiToHiraganaDAO";
-import RandomService from "./RandomService";
 
 const NUMBER_OF_ANSWERS = 4;
 
-const startNewGame = () => {
-    return {
-        questions : KanjiToHiraganaDAO.getQuestions()
-    }
-}
-
-const getXDifferentFrom = (x, from) => {
-    if(from.length < x){
-        x = from.length;
-    }
-    const answer = [];
-    const taken = new Set();
-    while(taken.size < x){
-        const possibleAnswer = from[RandomService.getRandomInt(0, from.length)];
-        if(!taken.has(possibleAnswer)){
-            answer.push(possibleAnswer)
-            taken.add(possibleAnswer);
-        }
-    }
-    return answer;
-}
-
-const withInPositionX = (x, what, collection) => {
-    const newCollection = [];
-    let j = 0;
-    for(let i = 0; i < NUMBER_OF_ANSWERS; i++){
-        if(i === x){
-            newCollection.push(what);
-        } else {
-            newCollection.push(collection[j]);
-            j += 1;
-        }
-    }
-    return newCollection;
-}
-
-const getNextQuestion = (questions) => {
-    const question = questions[RandomService.getRandomInt(0, questions.length)];
-    const answerData = KanjiToHiraganaDAO.getPossibleAnswers(question.id);
-    const wrongAnswers = getXDifferentFrom(NUMBER_OF_ANSWERS, answerData.incorrect);
-    const correctAnswerPosition = RandomService.getRandomInt(0, NUMBER_OF_ANSWERS);
-    return {
-        id       : question.id,
-        question : question.question,
-        answers  : withInPositionX(correctAnswerPosition, answerData.correct, wrongAnswers)
-    };
-}
-
-const checkIfCorrectAnswer = (id, answer) => {
-    const answerData = KanjiToHiraganaDAO.getPossibleAnswers(id);
-    return answer === answerData.correct;
-}
-
-const KanjiToHiraganaService = {
-    startNewGame,
-    getNextQuestion,
-    checkIfCorrectAnswer
-}
+const KanjiToHiraganaService = _QuestionsWithPossibleAnswersService(KanjiToHiraganaDAO, NUMBER_OF_ANSWERS);
 
 export default KanjiToHiraganaService;
---
+++ src/logic/_QuestionsWithPossibleAnswersService.js
@@ -0,0 +1,63 @@
+import RandomService from "./RandomService";
+
+const startNewGame = (store) => {
+    return {
+        questions : store.getQuestions()
+    }
+}
+
+const getXDifferentFrom = (x, from) => {
+    if(from.length < x){
+        x = from.length;
+    }
+    const answer = [];
+    const taken = new Set();
+    while(taken.size < x){
+        const possibleAnswer = from[RandomService.getRandomInt(0, from.length)];
+        if(!taken.has(possibleAnswer)){
+            answer.push(possibleAnswer)
+            taken.add(possibleAnswer);
+        }
+    }
+    return answer;
+}
+
+const withInPositionX = (x, what, collection) => {
+    const newCollection = [];
+    const newCollectionLength = collection.length + 1;
+    let j = 0;
+    for(let i = 0; i < newCollectionLength; i++){
+        if(i === x){
+            newCollection.push(what);
+        } else {
+            newCollection.push(collection[j]);
+            j += 1;
+        }
+    }
+    return newCollection;
+}
+
+const getNextQuestion = (store, number_of_answers, questions) => {
+    const question = questions[RandomService.getRandomInt(0, questions.length)];
+    const answerData = store.getPossibleAnswers(question.id);
+    const wrongAnswers = getXDifferentFrom(number_of_answers, answerData.incorrect);
+    const correctAnswerPosition = RandomService.getRandomInt(0, number_of_answers);
+    return {
+        id       : question.id,
+        question : question.question,
+        answers  : withInPositionX(correctAnswerPosition, answerData.correct, wrongAnswers)
+    };
+}
+
+const checkIfCorrectAnswer = (store, id, answer) => {
+    const answerData = store.getPossibleAnswers(id);
+    return answer === answerData.correct;
+}
+
+const _QuestionsWithPossibleAnswersService = (store, number_of_answers) => ({
+    startNewGame            : ()            => startNewGame(store),
+    getNextQuestion         : (questions)   => getNextQuestion(store, number_of_answers, questions),
+    checkIfCorrectAnswer    : (id, answer)  => checkIfCorrectAnswer(store, id, answer)
+});
+
+export default _QuestionsWithPossibleAnswersService;
