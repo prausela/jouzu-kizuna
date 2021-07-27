19ee92bf927a4b012f4f0e32048831d62fe039fa
Paula Oseroff <oseroffp@gmail.com>
2021-07-27 19:05:26 -0300

    Add TeFormDAO

---
+++ src/data/access/TeFormDAO.js
@@ -0,0 +1,6 @@
+import TeForm from '../store/te_form.json';
+import _QuestionsWithPossibleAnswersDAO from './_QuestionsWithPossibleAnswersDAO';
+
+const TeFormDAO = _QuestionsWithPossibleAnswersDAO(TeForm);
+
+export default TeFormDAO;
