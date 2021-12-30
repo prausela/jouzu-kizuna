59eb59d49f4a0d58847cb7ffed884fb22b5ea615
Paula Oseroff <oseroffp@gmail.com>
2021-12-29 22:27:21 -0300

    Add new forms services to App.js
    DictionaryFormService, NaiFormService & TaFormService are added to services array in App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import Interface from './UI/Interface';
 // Services
 import KanjiToHiraganaService from './logic/KanjiToHiraganaService';
 import TeFormService from './logic/TeFormService';
+import DictionaryFormService from './logic/DictionaryFormService';
+import NaiFormService from './logic/NaiFormService';
+import TaFormService from './logic/TaFormService';
 
 const services = {
     "kanji_to_hiragana_game" : KanjiToHiraganaService,
-    "te_form_game"           : TeFormService
+    "te_form_game"           : TeFormService,
+    "dictionary_form_game"   : DictionaryFormService,
+    "nai_form_game"          : NaiFormService,
+    "ta_form_game"           : TaFormService
 };
 
 const App = () => {
