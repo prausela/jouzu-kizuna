4c327d8f7fd879467d5178396cb762c9ff05e26e
Paula Oseroff <oseroffp@gmail.com>
2021-12-29 22:54:11 -0300

    Add new forms selection
    Add options for Dictionary, Nai and Ta forms in MainMenuUI.js

--- src/UI/main_menu/MainMenuUI.js
+++ src/UI/main_menu/MainMenuUI.js
@@ -6,6 +6,9 @@ import './MainMenuUI.css';
 const MainMenuUI = (props) => {
     const switchToKanjiToHiraganMode    = props.switchToKanjiToHiraganMode;
     const switchToTeFormMode            = props.switchToTeFormMode;
+    const switchToDictionaryFormMode    = props.switchToDictionaryFormMode;
+    const switchToNaiFormMode           = props.switchToNaiFormMode;
+    const switchToTaFormMode            = props.switchToTaFormMode;
     return (
         <>
             <div className="flex-grow-1 mode mode1 d-grid p-2">
@@ -13,6 +16,15 @@ const MainMenuUI = (props) => {
             </div>
             <div className="flex-grow-1 mode mode2 d-grid p-2">
                 <Button variant="outline-dark" onClick={switchToTeFormMode} active>Forma て</Button>
+            </div>
+            <div className="flex-grow-1 mode mode3 d-grid p-2">
+                <Button variant="outline-dark" onClick={switchToDictionaryFormMode} active>Forma じしょ</Button>
+            </div>
+            <div className="flex-grow-1 mode mode4 d-grid p-2">
+                <Button variant="outline-dark" onClick={switchToNaiFormMode} active>Forma ない</Button>
+            </div>
+            <div className="flex-grow-1 mode mode5 d-grid p-2">
+                <Button variant="outline-dark" onClick={switchToTaFormMode} active>Forma た</Button>
             </div>
         </>
     )
