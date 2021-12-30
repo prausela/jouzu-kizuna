e5d701f190c5a078039d11ce5ba83299b889ea03
Paula Oseroff <oseroffp@gmail.com>
2021-12-29 22:52:59 -0300

    Add form manipulation in MainMenuController

--- src/UI/controllers/MainMenuController.js
+++ src/UI/controllers/MainMenuController.js
@@ -4,10 +4,16 @@ import MainMenuUI from '../main_menu/MainMenuUI';
 const MainMenuController = (props) => {
     const switchToKanjiToHiraganMode    = () => props.switchToGameMode("kanji_to_hiragana_game");
     const switchToTeFormMode            = () => props.switchToGameMode("te_form_game");
+    const switchToDictionaryFormMode    = () => props.switchToGameMode("dictionary_form_game");
+    const switchToNaiFormMode           = () => props.switchToGameMode("nai_form_game");
+    const switchToTaFormMode            = () => props.switchToGameMode("ta_form_game");
     return (
         <MainMenuUI 
             switchToKanjiToHiraganMode={switchToKanjiToHiraganMode}
             switchToTeFormMode={switchToTeFormMode}
+            switchToDictionaryFormMode={switchToDictionaryFormMode}
+            switchToNaiFormMode={switchToNaiFormMode}
+            switchToTaFormMode={switchToTaFormMode}
         />
     )
 }
