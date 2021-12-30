077bcfdb2a58a9aafaad22be64188ae27a3b5351
Paula Oseroff <oseroffp@gmail.com>
2021-12-29 21:55:51 -0300

    Begin more forms feature: Dictionary Form
    Add dictionary_form.js store and DictionaryFormDAO

---
+++ src/data/access/DictionaryFormDAO.js
@@ -0,0 +1,6 @@
+import DictionaryForm from '../store/dictionary_form.json';
+import _QuestionsWithPossibleAnswersDAO from './_QuestionsWithPossibleAnswersDAO';
+
+const DictionaryFormDAO = _QuestionsWithPossibleAnswersDAO(DictionaryForm);
+
+export default DictionaryFormDAO;
