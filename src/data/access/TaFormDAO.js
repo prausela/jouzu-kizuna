229b1f45a00be333034a59a6b2d5f3efff2fb37f
Paula Oseroff <oseroffp@gmail.com>
2021-12-29 21:58:16 -0300

    Begin more forms feature: Ta Form
    Add ta_form.js store and TaFormDAO

---
+++ src/data/access/TaFormDAO.js
@@ -0,0 +1,6 @@
+import TaForm from '../store/ta_form.json';
+import _QuestionsWithPossibleAnswersDAO from './_QuestionsWithPossibleAnswersDAO';
+
+const TaFormDAO = _QuestionsWithPossibleAnswersDAO(TaForm);
+
+export default TaFormDAO;
