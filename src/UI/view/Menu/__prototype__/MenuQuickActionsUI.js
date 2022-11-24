96b822b6e8222f84c93e522b69f05feffe6dee5f
Paula Oseroff <oseroffp@gmail.com>
2022-11-24 00:06:51 -0300

    Add MenuQuickActionUI prototype

--- src/UI/view/Menu/__prototype__/MenuQuickActionsUI.js
+++ src/UI/view/Menu/__prototype__/MenuQuickActionsUI.js
@@ -1,9 +1,39 @@
 import React from "react";
 
-const MenuQuickActionsUI = () => {
+import QuickActionButtonUI from "../../Buttons/QuickActionButtonUI";
+
+const MenuQuickActionsUI = ({actions, className}) => {
     return (
-        <>
-        </>
+        <div className={(className ? className + " " : "") + "d-flex align-items-center justify-content-between"}>
+            {
+                actions ? actions.map((action, index) => {
+                    return action.show ? (
+                        <QuickActionButtonUI 
+                            name={action.name}
+                            icon={action.icon}
+                            className="flex-grow-1 flex-shrink-1"
+                            style={{
+                                flexBasis: 0,
+                                maxWidth: "8rem"
+                            }}
+                            show={action.show}
+                            disabled={action.disabled}
+                            key={index}
+                        />) : (
+                        <QuickActionButtonUI
+                            className="flex-grow-1 flex-shrink-1"
+                            style={{
+                                flexBasis: 0,
+                                maxWidth: "8rem"
+                            }}
+                            show={action.show}
+                            disabled={action.disabled}
+                            key={index}
+                        />
+                    )
+                }) : ""
+            }
+        </div>
     )
 }
 
